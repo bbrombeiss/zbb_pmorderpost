3835a66d8e5aacd2e206a39dfcab8b486eb3a0c3
Boris Bengelbert <blackfriday456@proton.me>
2025-01-14 18:46:09 +0000

    fragment

---
+++ webapp/ext/fragment/JsonViewer.js
@@ -0,0 +1,13 @@
+sap.ui.define(
+    ["sap/fe/core/AppComponent",
+    "sap/ui/codeeditor/CodeEditor"], // New
+    function (Component, CodeEditor) {
+        "use strict";
+
+        return Component.extend("workforce.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
